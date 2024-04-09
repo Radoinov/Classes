8b87bb92967a625aff234b4a728cacc059bbde74
Radoinov <radoinov08@gmail.com>
2024-04-09 23:35:58 +0300

    Domashna

---
+++ Zadacha 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System;
+
+namespace Zadacha_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            Person pesho = new Person();
+            pesho.name = "Pesho";
+            pesho.age = 20;
+            pesho.Eat();
+            Person gosho = new Person();
+            gosho.name = "gOsho";
+            gosho.age = 18;
+            gosho.Eat();
+            Person stamat = new Person();
+            stamat.name = "Stamata";
+            stamat.age = 43;
+            stamat.Eat();
+        }
+    }
+}
